1b69c3a1880fa269679e990c3cb80f49061d1d1c
Jan Kolomaznik <honza.kolomaznik@gmail.com>
2024-04-25 21:38:02 +0200

    WebGL example.

---
+++ 08_webgl/rotating_cube.js
@@ -0,0 +1,172 @@
+/*============= Creating a canvas =================*/
+var canvas = document.getElementById('webglCanvas');
+// gl = canvas.getContext('experimental-webgl');
+
+var gl = canvas.getContext('webgl');
+
+if (!gl) {
+	alert('WebGL not supported, falling back on experimental-webgl');
+	gl = canvas.getContext('experimental-webgl');
+}
+if (!gl) {
+	alert('Your browser does not support WebGL');
+}
+
+
+/*============ Defining and storing the geometry =========*/
+
+var vertices = [
+	-1,-1,-1, 1,-1,-1, 1, 1,-1, -1, 1,-1,
+	-1,-1, 1, 1,-1, 1, 1, 1, 1, -1, 1, 1,
+	-1,-1,-1, -1, 1,-1, -1, 1, 1, -1,-1, 1,
+	1,-1,-1, 1, 1,-1, 1, 1, 1, 1,-1, 1,
+	-1,-1,-1, -1,-1, 1, 1,-1, 1, 1,-1,-1,
+	-1, 1,-1, -1, 1, 1, 1, 1, 1, 1, 1,-1, 
+];
+
+var colors = [
+	5,3,7, 5,3,7, 5,3,7, 5,3,7,
+	1,1,3, 1,1,3, 1,1,3, 1,1,3,
+	0,0,1, 0,0,1, 0,0,1, 0,0,1,
+	1,0,0, 1,0,0, 1,0,0, 1,0,0,
+	1,1,0, 1,1,0, 1,1,0, 1,1,0,
+	0,1,0, 0,1,0, 0,1,0, 0,1,0
+];
+
+var indices = [
+	0,1,2, 0,2,3, 4,5,6, 4,6,7,
+	8,9,10, 8,10,11, 12,13,14, 12,14,15,
+	16,17,18, 16,18,19, 20,21,22, 20,22,23 
+];
+
+// Create and store data into vertex buffer
+var vertex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+// Create and store data into color buffer
+var color_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+// Create and store data into index buffer
+var index_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+/*=================== Shaders =========================*/
+
+var vertCode = 'attribute vec3 position;'+
+	'uniform mat4 Pmatrix;'+
+	'uniform mat4 Vmatrix;'+
+	'uniform mat4 Mmatrix;'+
+	'attribute vec3 color;'+//the color of the point
+	'varying vec3 vColor;'+
+
+	'void main(void) { '+//pre-built function
+	'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
+	'vColor = color;'+
+	'}';
+
+var fragCode = 'precision mediump float;'+
+	'varying vec3 vColor;'+
+	'void main(void) {'+
+	'gl_FragColor = vec4(vColor, 1.);'+
+	'}';
+
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vertCode);
+gl.compileShader(vertShader);
+
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fragCode);
+gl.compileShader(fragShader);
+
+var shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertShader);
+gl.attachShader(shaderProgram, fragShader);
+gl.linkProgram(shaderProgram);
+
+/* ====== Associating attributes to vertex shader =====*/
+var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
+var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
+var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+var position = gl.getAttribLocation(shaderProgram, "position");
+gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
+
+// Position
+gl.enableVertexAttribArray(position);
+gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+var color = gl.getAttribLocation(shaderProgram, "color");
+gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
+
+// Color
+gl.enableVertexAttribArray(color);
+gl.useProgram(shaderProgram);
+
+/*==================== MATRIX =====================*/
+
+var projectionMatrix = glMatrix.mat4.create();
+glMatrix.mat4.perspective(projectionMatrix, 
+                     Math.PI / 4,       // field of view in radians
+                     canvas.width / canvas.height, // aspect ratio
+                     0.1,               // near clip
+                     100.0);            // far clip
+
+// Model Matrix
+var modelMatrix = glMatrix.mat4.create(); // Starts with an identity matrix
+
+var viewMatrix = glMatrix.mat4.create();
+glMatrix.mat4.lookAt(viewMatrix, 
+	[0, 0, -10], // Camera is at (0,0,-10), in World Space
+	[0, 0, 0],   // and looks at the origin
+	[0, 1, 0]);  // Head is up (set to 0,-1,0 to look upside-down)
+
+
+/*==================== Rotation ====================*/
+
+// Function to update model matrix by rotating around the Z-axis
+function rotateZ(m, angle) {
+	glMatrix.mat4.rotateZ(m, m, angle);
+}
+
+// Function to update model matrix by rotating around the Y-axis
+function rotateY(m, angle) {
+	glMatrix.mat4.rotateY(m, m, angle);
+}
+
+// Function to update model matrix by rotating around the X-axis
+function rotateX(m, angle) {
+	glMatrix.mat4.rotateX(m, m, angle);
+}
+
+/*================= Drawing ===========================*/
+var time_old = 0;
+
+var animate = function(time) {
+
+	var dt = time-time_old;
+	rotateZ(modelMatrix, dt*0.005);//time
+	rotateY(modelMatrix, dt*0.002);
+	rotateX(modelMatrix, dt*0.003);
+	time_old = time;
+
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	gl.clearColor(0.5, 0.5, 0.5, 0.9);
+	gl.clearDepth(1.0);
+
+	gl.viewport(0.0, 0.0, canvas.width, canvas.height);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.uniformMatrix4fv(Pmatrix, false, projectionMatrix);
+	gl.uniformMatrix4fv(Vmatrix, false, viewMatrix);
+	gl.uniformMatrix4fv(Mmatrix, false, modelMatrix);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+	gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+	window.requestAnimationFrame(animate);
+}
+animate(0);
+
